0d11aa1fc5b4525b2aff028595bd55894c1fa8a3
Ibrahim <abrahamxman2@gmail.com>
2021-05-02 16:50:03 +0200

    update  record's controller using Mongoose

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 /** ROUTERS */
 const indexRouter = require("./routes/index");
@@ -20,26 +20,25 @@ const app = express();
 /** LOGGING */
 app.use(logger("dev"));
 
+/** ENV VARIABLES **/
+const dBURL = process.env.DB_URL;
+const dBPassword = process.env.DB_PASS;
+const dBUser = process.env.DB_USER;
+
 /**CONNECT TO DB */
-mongoose.connect("mongodb://localhost:27017/record-shop", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+    process.env.NODE_ENV == 'test' ?
+    'mongodb://localhost:27017/record-shop' :
+    `mongodb+srv://${dBUser}:${dBPassword}@${dBURL}`, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    });
 
 mongoose.connection.on("error", console.error);
 mongoose.connection.on("open", function() {
-  console.log("Database connection established...");
+    console.log("Database connection established...");
 });
-
-/** SETTING UP LOWDB */
-const adapter = new FileSync("data/db.json");
-const db = low(adapter);
-db.defaults({
-  records: [],
-  users: [],
-  orders: []
-}).write();
 
 /** REQUEST PARSERS */
 app.use(express.json());
@@ -58,17 +57,17 @@ app.use("/orders", ordersRouter);
 
 /** ERROR HANDLING */
 app.use(function(req, res, next) {
-  const error = new Error("Looks like something broke...");
-  error.status = 400;
-  next(error);
+    const error = new Error("Looks like something broke...");
+    error.status = 400;
+    next(error);
 });
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500).send({
-    error: {
-      message: err.message
-    }
-  });
+    res.status(err.status || 500).send({
+        error: {
+            message: err.message,
+        },
+    });
 });
 
 /** EXPORT PATH */
